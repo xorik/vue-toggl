3b7de379e39a5080ab9ce2e096991afda0f9ef83
 Andrey Smelov <xor29a@bk.ru>
2017-10-15 15:00:14 +0300

    Empty project with bootswatch, less and pug

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    .container
+        h3 Project name
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
+
+<style lang="less">
+  @import "~bootstrap/less/bootstrap.less";
+  @import "~bootswatch/yeti/variables.less";
+  @import "~bootswatch/yeti/bootswatch.less";
+</style>
